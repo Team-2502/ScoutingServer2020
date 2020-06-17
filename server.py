6a164255bc13bdacea12439984c9d55219545e9e
Ryan Alexander <ryandalex1@outlook.com>
2020-06-17 15:46:51 -0500

    Put the server in a try except block to report errors to Slack

--- server.py
+++ server.py
@@ -52,63 +52,70 @@ def run_server_comp():
             time.sleep(10)
 
         else:
-            current_unscouted_match = database.child('config').child('currentMatch').get().val()
+            try:
+                current_unscouted_match = database.child('config').child('currentMatch').get().val()
 
-            for temp_timd in rawTIMDs.each():
+                for temp_timd in rawTIMDs.each():
 
-                timd = calculateTIMD.calculate_timd(temp_timd.val(), temp_timd.key())
-                database.child("rawTIMDs").child(temp_timd.key()).remove()
-                team_num = temp_timd.key().split("-")[1]
-                calculateTeam.calculate_team(team_num, timd)
+                    timd = calculateTIMD.calculate_timd(temp_timd.val(), temp_timd.key())
+                    database.child("rawTIMDs").child(temp_timd.key()).remove()
+                    team_num = temp_timd.key().split("-")[1]
+                    calculateTeam.calculate_team(team_num, timd)
 
-                match_num = timd['match_number']
-                if match_num == current_unscouted_match:
-                    timds_for_current_match += 1
-                    if timds_for_current_match == 6:
-                        print("\nAll TIMDs for QM " + str(current_unscouted_match) + " synced\n")
-                        timds_for_current_match = 0
+                    match_num = timd['match_number']
+                    if match_num == current_unscouted_match:
+                        timds_for_current_match += 1
+                        if timds_for_current_match == 6:
+                            print("\nAll TIMDs for QM " + str(current_unscouted_match) + " synced\n")
+                            timds_for_current_match = 0
 
-                        export.export_spreadsheet()
-                        print("Data exported")
-                        export.upload_to_drive(" Post QM" + str(current_unscouted_match) + "Full Export")
-                        print("Data uploaded to Drive\n")
+                            export.export_spreadsheet()
+                            print("Data exported")
+                            export.upload_to_drive(" Post QM" + str(current_unscouted_match) + "Full Export")
+                            print("Data uploaded to Drive\n")
+                            slack_client.chat_postMessage(
+                                channel="UC3TC3PN3",
+                                text="All TIMDs for Match " + str(current_unscouted_match) + " processed and data exported"
+                            )
+                            database.child('config').child('currentMatch').set(current_unscouted_match + 1)
+
+                    elif match_num > current_unscouted_match:
+                        print("WARNING: MISSING TIMD FOR MATCH " + str(current_unscouted_match))
                         slack_client.chat_postMessage(
                             channel="UC3TC3PN3",
-                            text="All TIMDs for Match " + str(current_unscouted_match) + " processed and data exported"
+                            text="WARNING: TIMD for Match " + str(match_num) + " uploaded before Match " +
+                                 str(current_unscouted_match) + " had 6 TIMDs!"
                         )
-                        database.child('config').child('currentMatch').set(current_unscouted_match + 1)
+                        database.child('config').child('currentMatch').set(match_num)
+                        timds_for_current_match = 1
 
-                elif match_num > current_unscouted_match:
-                    print("WARNING: MISSING TIMD FOR MATCH " + str(current_unscouted_match))
-                    slack_client.chat_postMessage(
-                        channel="UC3TC3PN3",
-                        text="WARNING: TIMD for Match " + str(match_num) + " uploaded before Match " +
-                             str(current_unscouted_match) + " had 6 TIMDs!"
-                    )
-                    database.child('config').child('currentMatch').set(match_num)
-                    timds_for_current_match = 1
-
-                elif match_num < current_unscouted_match:
-                    print("WARNING: CALCULATING TIMD FROM PAST MATCH " + str(match_num))
-                    if get_num_timds_for_match(match_num, database) == 6:
-                        print("\nAll TIMDs for QM " + str(match_num) + " synced\n")
-                        timds_for_current_match = 0
-                        export.export_spreadsheet()
-                        print("Data exported")
-                        export.upload_to_drive(" Post QM" + str(match_num) + "Full Export")
-                        print("Data uploaded to Drive\n")
-                        slack_client.chat_postMessage(
-                            channel="UC3TC3PN3",
-                            text="All TIMDs for Match " + str(match_num) + "processed and data exported"
-                        )
-                    elif get_num_timds_for_match(match_num, database) > 6:
-                        slack_client.chat_postMessage(
-                            channel="UC3TC3PN3",
-                            text="More than 6 TIMDs processed for Match " + str(match_num)
-                        )
-                        # TODO Delete the extra one
-                    else:
-                        print("WARNING: Still missing " + str(6 - get_num_timds_for_match(match_num, database)) + " TIMDs for Match " + str(match_num))
+                    elif match_num < current_unscouted_match:
+                        print("WARNING: CALCULATING TIMD FROM PAST MATCH " + str(match_num))
+                        if get_num_timds_for_match(match_num, database) == 6:
+                            print("\nAll TIMDs for QM " + str(match_num) + " synced\n")
+                            timds_for_current_match = 0
+                            export.export_spreadsheet()
+                            print("Data exported")
+                            export.upload_to_drive(" Post QM" + str(match_num) + "Full Export")
+                            print("Data uploaded to Drive\n")
+                            slack_client.chat_postMessage(
+                                channel="UC3TC3PN3",
+                                text="All TIMDs for Match " + str(match_num) + "processed and data exported"
+                            )
+                        elif get_num_timds_for_match(match_num, database) > 6:
+                            slack_client.chat_postMessage(
+                                channel="UC3TC3PN3",
+                                text="More than 6 TIMDs processed for Match " + str(match_num)
+                            )
+                            # TODO Delete the extra one
+                        else:
+                            print("WARNING: Still missing " + str(6 - get_num_timds_for_match(match_num, database)) + " TIMDs for Match " + str(match_num))
+            except Exception as e:
+                slack_client.chat_postMessage(
+                    channel="UC3TC3PN3",
+                    text="Exception occurred: " + str(e)
+                )
+                exit()
 
 
 if __name__ == "__main__":
