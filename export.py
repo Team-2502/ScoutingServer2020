9c3d8c0e04e75144f7bfdff0d1370362413eedde
Ryan Alexander <ryandalex1@outlook.com>
2020-06-25 10:02:23 -0500

    Move to pygsheets for exporting

--- export.py
+++ export.py
@@ -1,51 +1,18 @@
+import pygsheets
+import pyrebase
 import os
 import ast
 
-import openpyxl
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-import pyrebase
-
-
-def upload_to_drive(filename):
-    gauth = GoogleAuth()
-    # Try to load saved client credentials
-    gauth.LoadCredentialsFile("oauthcreds.txt")
-
-    if gauth.credentials is None:
-        # Authenticate if they're not there
-
-        # https://stackoverflow.com/a/55876179
-        # This is what solved the issues:
-        gauth.GetFlow()
-        gauth.flow.params.update({'access_type': 'offline'})
-        gauth.flow.params.update({'approval_prompt': 'force'})
-
-        gauth.LocalWebserverAuth()
-
-    elif gauth.access_token_expired:
-        # Refresh them if expired
-        gauth.Refresh()
-
-    else:
-        # Initialize the saved creds
-        gauth.Authorize()
-    # Save the current credentials to a file
-    gauth.SaveCredentialsFile("oauthcreds.txt")
-
-    drive = GoogleDrive(gauth)
-
-    # https://stackoverflow.com/a/22934892
-    # https://stackoverflow.com/a/40236586
-    drive_file = drive.CreateFile({'title': filename,
-                                   "parents": [{"kind": "drive#fileLink", "id": os.environ['drive_file_id']}]})
-
-    # Read file and set it as a content of this instance.
-    drive_file.SetContentFile("export.xlsx")
-    drive_file.Upload()  # Upload the file.
-
 
 def export_spreadsheet():
+    client = pygsheets.authorize(service_account_env_var="g_drive_creds")
+
+    wb = client.open('Scouting Template 2021')
+    team_export = wb.worksheet_by_title("Test Team Export")
+    timd_export = wb.worksheet_by_title("Test TIMD Export")
+    team_export.clear()
+    timd_export.clear()
+
     firebase = pyrebase.initialize_app(ast.literal_eval(os.environ['firebase_info']))
     database = firebase.database()
 
@@ -69,76 +36,36 @@ def export_spreadsheet():
     headers = [totals, l3ms, SDs, maxes, team_abilities, percentages, p75s]  # [sykes]
     timdHeaders = [timdHeader, calculated, climb]
 
-    wb = openpyxl.load_workbook('export.xlsx')
+    all_team_headers = ['Number'] + [item for elem in [header[0] for header in headers] for item in elem]
+    all_timd_headers = ['Team', 'Match'] + [item for elem in [header[0] for header in timdHeaders] for item in elem]
 
-    for sheet in wb.worksheets:
-        wb.remove(sheet)
-
-    raw_team_sheet = wb.create_sheet('Raw Teams Export')
-    raw_team_sheet.cell(row=1, column=1).value = 'Number'
-
-    raw_timd_sheet = wb.create_sheet('Raw TIMD Export')
-    raw_timd_sheet.cell(row=1, column=1).value = 'Team'
-    raw_timd_sheet.cell(row=1, column=2).value = 'Match'
-
-    current_column = 3
-
-    for header in timdHeaders:
-        for key in header[0]:
-            raw_timd_sheet.cell(row=1, column=current_column).value = key
-            current_column += 1
-
-    current_column = 2
-
-    for header in headers:
-        for key in header[0]:
-            raw_team_sheet.cell(row=1, column=current_column).value = key
-            current_column += 1
-
-    current_timd_row = 2
-    current_team_row = 2
+    team_export_values = [all_team_headers]
+    timd_export_values = [all_timd_headers]
 
     for team in teams:
         team = team.val()
-        current_column = 2
-        raw_team_sheet.cell(row=current_team_row, column=1).value = team['teamNumber']
+        team_values = [team['teamNumber']]
 
         for header in headers:
             for key in header[0]:
-                raw_team_sheet.cell(row=current_team_row, column=current_column).value = team[header[1]][key]
-                current_column += 1
-
-        current_team_row += 1
+                team_values.append(team[header[1]][key])
+        team_export_values.append(team_values)
 
         for timd in team['timds']:
-            current_column = 3
-            raw_timd_sheet.cell(row=current_timd_row, column=1).value = timd['team_number']
-            raw_timd_sheet.cell(row=current_timd_row, column=2).value = timd['match_number']
+            timd_values = [timd['team_number'], timd['match_number']]
 
             if timd['header']['noShow']:
                 for key in timdHeader[0]:
-                    raw_timd_sheet.cell(row=current_timd_row, column=current_column).value = timd['header'][key]
-                    current_column += 1
+                    timd_values.append(timd['header'][key])
 
             else:
                 for header in timdHeaders:
                     for key in header[0]:
                         timd_component_header = timd[header[1]]
                         if key in timd_component_header.keys():
-                            raw_timd_sheet.cell(row=current_timd_row, column=current_column).value = timd_component_header[key]
-                        current_column += 1
+                            timd_values.append(timd_component_header[key])
 
-            current_timd_row += 1
+            timd_export_values.append(timd_values)
 
-    wb.save('export.xlsx')
-
-
-if __name__ == "__main__":
-    export_spreadsheet()
-
-
-# Download spreadsheet from excel
-# Open via openpyxl
-# Edit the two sheets with the updated data
-# Save the spreadsheet
-# Reupload the file with changes
+    team_export.insert_rows(0, values=team_export_values)
+    timd_export.insert_rows(0, values=timd_export_values)
