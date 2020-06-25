4ef4f5c0df922d9562fc7fca7c7fe9e89c6a7713
Ryan Alexander <ryandalex1@outlook.com>
2020-06-25 10:35:36 -0500

    Create assignment file in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ import pyrebase
 import os
 import ast
 
+import tablets.createAssignmentFile
+
 firebase = pyrebase.initialize_app(ast.literal_eval(os.environ['firebase_info']))
 database = firebase.database()
 
 database.child('config').child('currentMatch').set(1)
+tablets.createAssignmentFile.main()
+
--- tablets/createAssignmentFile.py
+++ tablets/createAssignmentFile.py
@@ -3,43 +3,44 @@ import pyrebase
 import os
 import ast
 
-firebase = pyrebase.initialize_app(ast.literal_eval(os.environ['firebase_info']))
-database = firebase.database()
 
-# Setup for tbapy
-tba = tbapy.TBA(os.environ['tba_api_key'])
-event = os.environ['event']
+def main():
+    firebase = pyrebase.initialize_app(ast.literal_eval(os.environ['firebase_info']))
+    database = firebase.database()
 
-# Get a list of all qualifying matches at an event
-try:
-    matches = [match for match in tba.event_matches(event, simple=True) if match['comp_level'] == 'qm']
+    # Setup for tbapy
+    tba = tbapy.TBA(os.environ['tba_api_key'])
+    event = os.environ['event']
 
-# TODO Make this except clause more specfic
-except:
-    print("Error getting matches from TBA, check event and API keys.")
-    exit(1)
+    # Get a list of all qualifying matches at an event
+    try:
+        matches = [match for match in tba.event_matches(event, simple=True) if match['comp_level'] == 'qm']
 
-full_assignments = {}
-ds_order = ["Red 1", "Red 2", "Red 3", "Blue 1", "Blue 2", "Blue 3"]
+    except:
+        print("Error getting matches from TBA, check event and API keys.")
+        exit(1)
 
-for match in matches:
-    # Query TBA for info about each match
-    match_num = match['match_number']
-    redTeams = match['alliances']['red']['team_keys']
-    redTeams = [int(team[3:]) for team in redTeams]
-    blueTeams = match['alliances']['blue']['team_keys']
-    blueTeams = [int(team[3:]) for team in blueTeams]
-    teams = redTeams + blueTeams
+    full_assignments = {}
+    ds_order = ["Red 1", "Red 2", "Red 3", "Blue 1", "Blue 2", "Blue 3"]
 
-    assignments = {}
-    numScouts = 6
-    scouts = ['scout' + str(x) for x in range(1, numScouts + 1)]
+    for match in matches:
+        # Query TBA for info about each match
+        match_num = match['match_number']
+        redTeams = match['alliances']['red']['team_keys']
+        redTeams = [int(team[3:]) for team in redTeams]
+        blueTeams = match['alliances']['blue']['team_keys']
+        blueTeams = [int(team[3:]) for team in blueTeams]
+        teams = redTeams + blueTeams
 
-    # Assign each scout to a team
-    for i in range(len(scouts)):
-        assignments[scouts[i]] = {'team': teams[(i % 6)], 'alliance': (ds_order[(i % 6)])}
+        assignments = {}
+        numScouts = 6
+        scouts = ['scout' + str(x) for x in range(1, numScouts + 1)]
 
-    full_assignments["QM "+str(match_num)] = assignments
+        # Assign each scout to a team
+        for i in range(len(scouts)):
+            assignments[scouts[i]] = {'team': teams[(i % 6)], 'alliance': (ds_order[(i % 6)])}
 
-# Upload assignments to Firebase
-database.child("config").child("scoutAssignments").set(full_assignments)
+        full_assignments["QM "+str(match_num)] = assignments
+
+    # Upload assignments to Firebase
+    database.child("config").child("scoutAssignments").set(full_assignments)
